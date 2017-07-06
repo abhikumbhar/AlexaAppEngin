7b2b91d1bc16a923d7fa7f802458c4c038c58aab
akumbhar <akumbhar@digitalriver.com>
2017-07-06 15:26:20 +0530

    commiting sysout

--- src/main/java/com/mycompany/myapp/HelloAppEngine.java
+++ src/main/java/com/mycompany/myapp/HelloAppEngine.java
@@ -42,6 +42,13 @@ public class HelloAppEngine extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
 	  
+	  
+	  Properties properties = System.getProperties();
+
+	    response.setContentType("text/plain");
+	    response.getWriter().println("Hello App Engine Get - Standard using "
+	        + SystemProperty.version.get() + " Java " + properties.get("java.specification.version"));
+	  
 	// Configure logging to output to the console with default level of INFO
 	    BasicConfigurator.configure();
 
@@ -99,6 +106,71 @@ public class HelloAppEngine extends HttpServlet {
     response.getWriter().println("Hello App Engine - Standard using "
         + SystemProperty.version.get() + " Java " + properties.get("java.specification.version"));
 */  }
+  
+  
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+	  
+	    Properties properties = System.getProperties();
+
+	    response.setContentType("text/plain");
+	    response.getWriter().println("Hello App Engine - Standard using "
+	        + SystemProperty.version.get() + " Java " + properties.get("java.specification.version"));
+	  
+	// Configure logging to output to the console with default level of INFO
+	    BasicConfigurator.configure();
+
+	    // Configure server and its associated servlets
+	    Server server = new Server();
+	    SslConnectionFactory sslConnectionFactory = new SslConnectionFactory();
+	    System.out.println("SSL protocol="+sslConnectionFactory.getProtocol());
+	    
+	    SslContextFactory sslContextFactory = sslConnectionFactory.getSslContextFactory();
+	    
+	   sslContextFactory.setKeyStorePath("src/main/webapp/MyNewJks.jks");
+	    //sslContextFactory.setKeyStorePath("./MyNewJks.jks");
+	    
+
+	    //sslContextFactory.setKeyStorePassword(System.getProperty("javax.net.ssl.keyStorePassword"));
+	    sslContextFactory.setKeyStorePassword("test123");
+	    sslContextFactory.setIncludeCipherSuites(Sdk.SUPPORTED_CIPHER_SUITES);
+	    
+	    HttpConfiguration httpConf = new HttpConfiguration();
+	    
+	    
+	    httpConf.setSecurePort(PORT);
+	    httpConf.setSecureScheme(HTTPS_SCHEME);
+	    httpConf.addCustomizer(new SecureRequestCustomizer());
+	    
+	    HttpConnectionFactory httpConnectionFactory = new HttpConnectionFactory(httpConf);
+	    
+	    ServerConnector serverConnector =
+	            new ServerConnector(server,sslConnectionFactory, httpConnectionFactory);
+	    serverConnector.setPort(PORT);
+
+	    Connector[] connectors = new Connector[1];
+	    connectors[0] = serverConnector;
+	    server.setConnectors(connectors);
+
+	    
+	    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+	    context.setContextPath("/");
+	    server.setHandler(context);
+	    context.addServlet(new ServletHolder(createServlet(new HelloWorldSpeechlet())), "/hello");
+	    try {
+			server.start();
+		    server.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	  
+
+
+  }
   
   private static SpeechletServlet createServlet(final Speechlet speechlet) {
 	   SpeechletServlet servlet = new SpeechletServlet();
